cd4ec99310a09c5d7a9103d9dd4565b650b9f1b4
Nicolas Longuet Marx <nkd2120@columbia.edu>
2023-02-01 02:26:37 +0100

    clip issue_adj

--- tbip_tf2_issue_adj5.py
+++ tbip_tf2_issue_adj5.py
@@ -459,7 +459,7 @@ class TBIP(tf.keras.Model):
       self.num_samples, seed=seed)
     
     # let's clip the issue adjustment between -5 and 5 
-    #issue_adjustment_samples = tf.clip_by_value(issue_adjustment_samples, -0.5, 0.5)
+    issue_adjustment_samples = tf.clip_by_value(issue_adjustment_samples, -1, 1)
     samples = [document_samples, objective_topic_samples,
                ideological_topic_samples, ideal_point_samples,
                issue_adjustment_samples, author_verbosity_samples]
@@ -562,7 +562,7 @@ class TBIP(tf.keras.Model):
     
     
     
-    #issue_adjustment_loc = tf.clip_by_value(issue_adjustment_loc, -0.5, 0.5)
+    issue_adjustment_loc = tf.clip_by_value(issue_adjustment_loc, -1, 1)
     
     author_verbosity_loc = tf.gather(
       self.author_verbosity_distribution.location, 
@@ -1091,10 +1091,10 @@ def main(argv):
       checkpoint.seed.assign(seed)
     
       # update the clipped issue_adjustment 
-      #all_issue_adjustement_loc = self.issue_adjustment_distribution.location
-      #all_issue_adjustement_loc = tf.clip_by_value(all_issue_adjustement_loc, -1, 1)
-      #self.issue_adjustment_distribution.location.assign(all_issue_adjustement_loc)
-      #assert self.issue_adjustment_distribution.location.numpy.max()<=1
+      all_issue_adjustement_loc = self.issue_adjustment_distribution.location
+      all_issue_adjustement_loc = tf.clip_by_value(all_issue_adjustement_loc, -1, 1)
+      self.issue_adjustment_distribution.location.assign(all_issue_adjustement_loc)
+      assert self.issue_adjustment_distribution.location.numpy.max()<=1
 
     sec_per_step = (time.time() - start_time) / (step + 1)
     sec_per_epoch = (time.time() - start_time) / (epoch - start_epoch)
